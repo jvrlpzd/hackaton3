9debd9f5f13e0b09054a0ded9ac13b13a4dd8580
ryanrasal <ryan.beaujot@outlook.fr>
2023-01-11 17:11:08 +0100

    formReservation

--- frontend/src/components/FormReservation.jsx
+++ frontend/src/components/FormReservation.jsx
@@ -1,63 +1,69 @@
 import React, { useState } from "react";
 
 function FormReservation() {
-  const [type, setType] = useState("");
+  const [city, setCity] = useState("");
+
   return (
     <div className="flex items-center justify-center p-12">
       <div className=" mx-auto w-full max-w-[550px]">
         <form action="https://formbold.com/s/FORM_ID" method="POST">
-          <div className="-mx-3 flex flex-wrap">
-            <div className=" w-full px-3 sm:w-1/2">
-              <div className="mb-5">
-                <select
-                  onChange={(e) => setType(e.target.value)}
-                  className="w-auto p-2.5 md:my-2 md:mx-2 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-                >
-                  <option>-Type-</option>
-                  <option value="Citadine" className=" sm:text-sm md:text-lg">
-                    Citadine
-                  </option>
-                  <option value="SUV" className=" sm:text-sm md:text-lg">
-                    SUV
-                  </option>
-                  <option value="Sedan" className=" sm:text-sm md:text-lg">
-                    Sedan
-                  </option>
-                  <option value="Coupé" className=" sm:text-sm md:text-lg">
-                    Coupé
-                  </option>
-                  <option value="Break" className=" sm:text-sm md:text-lg">
-                    Break
-                  </option>
-                </select>
+          <div className="flex">
+            <div className="-mx-3 flex">
+              <div className=" w-full px-3 sm:w-1/2">
+                <div className="mb-5">
+                  <select
+                    onChange={(e) => setCity(e.target.value)}
+                    className="w-auto p-2.5 md:my-2 md:mx-2 text-gray-500 bg-white border rounded-md shadow-sm outline-none  focus:border-indigo-600"
+                  >
+                    <option>-City-</option>
+                    <option value="Paris" className=" sm:text-sm md:text-lg">
+                      Paris
+                    </option>
+                    <option value="Lille" className=" sm:text-sm md:text-lg">
+                      Lille
+                    </option>
+                    <option value="Lyon" className=" sm:text-sm md:text-lg">
+                      Lyon
+                    </option>
+                    <option value="Bordeaux" className=" sm:text-sm md:text-lg">
+                      Bordeaux
+                    </option>
+                    <option value="Monaco" className=" sm:text-sm md:text-lg">
+                      Monaco
+                    </option>
+                  </select>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="-mx-3 flex flex-wrap">
-            <div className="w-full px-3 sm:w-1/2">
-              <div className="mb-5">
-                <label
-                  htmlFor="date"
-                  className="mb-3 block text-base font-medium text-[#07074D]"
-                >
-                  Date
-                </label>
-                <input
-                  type="date"
-                  name="date"
-                  id="date"
-                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <button
-              type="button"
-              className="hover:shadow-form rounded-md bg-[#15133C] py-3 px-8 text-center text-base font-semibold text-white outline-none hover:shadow hover:bg-[#FFFFFF] hover:text-[#15133C] hover:border hover:border-[#15133C]"
-            >
-              Search
+            <button type="button" onClick={() => console.log(city)}>
+              TEST
             </button>
+            <div className="-mx-3 flex">
+              <div className="w-full px-3 sm:w-1/2">
+                <div className="mb-5">
+                  <label
+                    htmlFor="date"
+                    className="mb-3 block text-base font-medium text-[#07074D]"
+                  >
+                    Date
+                  </label>
+                  <input
+                    type="date"
+                    name="date"
+                    id="date"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280]  focus:border-[#6A64F1] focus:shadow-md"
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="hover:shadow-form rounded-md bg-[#15133C] py-3 px-8 text-center text-base font-semibold text-white outline-none hover:shadow hover:bg-[#FFFFFF] hover:text-[#15133C] hover:border hover:border-[#15133C]"
+              >
+                Search
+              </button>
+            </div>
           </div>
         </form>
       </div>
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -22,6 +22,8 @@ function SideBar() {
 
   const [brand, setBrand] = useState("");
 
+  const [type, setType] = useState("");
+
   const [mileage, setMileage] = useState("");
 
   const [fuel, setFuel] = useState("");
@@ -41,6 +43,27 @@ function SideBar() {
             {brands.name}
           </option>
         ))}
+      </select>
+      <select
+        onChange={(e) => setType(e.target.value)}
+        className="w-auto p-2.5 md:my-2 md:mx-2 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+      >
+        <option>-Type-</option>
+        <option value="Citadine" className=" sm:text-sm md:text-lg">
+          Citadine
+        </option>
+        <option value="SUV" className=" sm:text-sm md:text-lg">
+          SUV
+        </option>
+        <option value="Sedan" className=" sm:text-sm md:text-lg">
+          Sedan
+        </option>
+        <option value="Coupé" className=" sm:text-sm md:text-lg">
+          Coupé
+        </option>
+        <option value="Break" className=" sm:text-sm md:text-lg">
+          Break
+        </option>
       </select>
       <select
         onChange={(e) => setMileage(e.target.value)}
@@ -73,7 +96,10 @@ function SideBar() {
         </option>
       </select>
 
-      <button onClick={() => console.log(brand, mileage, fuel)} type="button">
+      <button
+        onClick={() => console.log(brand, mileage, fuel, type)}
+        type="button"
+      >
         Test
       </button>
     </div>
