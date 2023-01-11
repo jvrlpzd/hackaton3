00e184f498f627117ed2ea7c013bf285cb5080ba
ryanrasal <ryan.beaujot@outlook.fr>
2023-01-11 20:04:26 +0100

    update Home Page and Formulaire

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import bannerPicture from "../assets/Banner.jpg";
+import bannerPicture from "../assets/banner.png";
 
 function Banner() {
   return (
     <div className="flex justify-center md:justify-center">
-      <h1 className="absolute mt-6 md:mt-28 text-4xl md:text-6xl z-0">
+      <h1 className="absolute mt-4 md:mt-18 text-4xl md:text-6xl z-0">
         WILD CAR RENT
       </h1>
       <img src={bannerPicture} alt="bannerPicture" className="h-3/4 w-full" />
