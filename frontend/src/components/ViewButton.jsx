caeffa2aeb639ba3b4bb01a2e16fd06b74545eb7
Chloé Bidau <chloebidau@hotmail.fr>
2023-01-11 16:19:07 +0100

    decomposition des composants de History

---
+++ frontend/src/components/ViewButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ViewButton() {
+  return (
+    <button
+      type="button"
+      className="bg-[#003DA5] text-white m-3 py-1 px-4 rounded-lg shadow-lg md:h-14 md:w-44 md:text-lg hover:shadow hover:bg-[#FFC927] hover:text-black"
+    >
+      VOIR
+    </button>
+  );
+}
+
+export default ViewButton;
